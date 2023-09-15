0bceda3e217b36c5ac7f16e89e9f76aada5c0dcb
tauseefm26 <144789230+tauseefm26@users.noreply.github.com>
2023-09-15 11:41:57 +0530

    Update winepr.py

--- winepr.py
+++ winepr.py
@@ -1,6 +1,13 @@
-print("this is for wine quality pred model")
-Print("adding the ref of delhi team")
-echo "hi write the name of wine tree"
-read me
+
+#!/bin/bash
+# Calculate the sum of two integers with pre initialize values
+# in a shell script
+ 
+a=10
+b=20
+ 
+sum=$(( $a + $b ))
+ 
+echo "Sum is: $sum"
 print("Name of the wine is $name")
 print("Done by github account to discuss about pull and fetch")
