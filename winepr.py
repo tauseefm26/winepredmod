5c027420330eb4d404a7f8340d8da3975f214674
Mohd Tauseef <mohdtauseef2690gmail.com>
2023-09-15 10:51:44 +0530

    modified the code

--- winepr.py
+++ winepr.py
@@ -1,5 +1,5 @@
 print("this is for wine quality pred model")
 Print("adding the ref of delhi team")
-echo "hi write the name of wine"
+echo "hi write the name of wine tree"
 read me
 print("Name of the wine is $name")
