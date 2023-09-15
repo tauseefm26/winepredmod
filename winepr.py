41df77b92e61656f5117a4e2cb3268479c9fa454
tauseefm26 <144789230+tauseefm26@users.noreply.github.com>
2023-09-15 11:22:58 +0530

    Update winepr.py

--- winepr.py
+++ winepr.py
@@ -3,3 +3,4 @@ Print("adding the ref of delhi team")
 echo "hi write the name of wine tree"
 read me
 print("Name of the wine is $name")
+print("Done by github account to discuss about pull and fetch")
