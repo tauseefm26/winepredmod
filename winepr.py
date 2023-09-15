e94132916e07f5e0eca76a4b8e8e9c0662bff602
Mohd Tauseef <mohdtauseef2690gmail.com>
2023-09-15 10:49:44 +0530

    Hi Dbda project

--- winepr.py
+++ winepr.py
@@ -1,4 +1,5 @@
-print("this is for winf quality pred model")
+print("this is for wine quality pred model")
+Print("adding the ref of delhi team")
 echo "hi write the name of wine"
 read me
 print("Name of the wine is $name")
